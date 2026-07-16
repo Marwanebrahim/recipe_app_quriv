d4cd553f88e429863f7143a8dfa1d70776c8db6e
marwan ebrahim <marwanebrahim2006@gmail.com>
2026-07-16 20:45:25 +0300

    feat: Implement RecipeEntity class with properties and constructor for recipe data mapping

--- lib/feature/home/data/mapper/recipe_model_mapper.dart
+++ lib/feature/home/data/mapper/recipe_model_mapper.dart
@@ -2,5 +2,15 @@ import 'package:recipe_app_quriv/feature/home/data/models/recipe_model.dart';
 import 'package:recipe_app_quriv/feature/home/domain/entity/recipe_entity.dart';
 
 extension RecipeModelMapper on RecipeModel {
-  RecipeEntity toEntity() => RecipeEntity();
+  RecipeEntity toEntity() => RecipeEntity(
+    id: id,
+    name: name,
+    ingredients: ingredients,
+    instructions: instructions,
+    prepTimeMinutes: prepTimeMinutes,
+    cookTimeMinutes: cookTimeMinutes,
+    servings: servings,
+    image: image,
+    rating: rating,
+  );
 }
--- lib/feature/home/domain/entity/recipe_entity.dart
+++ lib/feature/home/domain/entity/recipe_entity.dart
@@ -1,1 +1,23 @@
-class RecipeEntity {}
+class RecipeEntity {
+  final int id;
+  final String name;
+  final List<String> ingredients;
+  final List<String> instructions;
+  final int prepTimeMinutes;
+  final int cookTimeMinutes;
+  final int servings;
+  final String image;
+  final double rating;
+
+  RecipeEntity({
+    required this.id,
+    required this.name,
+    required this.ingredients,
+    required this.instructions,
+    required this.prepTimeMinutes,
+    required this.cookTimeMinutes,
+    required this.servings,
+    required this.image,
+    required this.rating,
+  });
+}
